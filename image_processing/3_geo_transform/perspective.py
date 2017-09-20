5826a78effd6467dc34adc298196d092ba715e43
e-Yantra <e-yantra@e-yanntra.org>
2017-09-20 10:12:40 +0530

    added the esc key for exiting the app

--- image_processing/3_geo_transform/perspective.py
+++ image_processing/3_geo_transform/perspective.py
@@ -27,6 +27,10 @@ cv2.imshow('output', dst)
 
 ############################################
 ## Close and exit
-cv2.waitKey(0)
+while(1):
+       key= cv2.waitKey(1)
+       if (key & 0xFF) == 27:
+           break
+
 cv2.destroyAllWindows()
 ############################################
--- image_processing/3_geo_transform/translation.py
+++ image_processing/3_geo_transform/translation.py
@@ -24,6 +24,10 @@ cv2.imshow('transform', dst)
 
 ############################################
 ## Close and exit
-cv2.waitKey(0)
+while(1):
+       key= cv2.waitKey(1)
+       if (key & 0xFF) == 27:
+           break
+
 cv2.destroyAllWindows()
 ############################################
